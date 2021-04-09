afe0fda0799a229fc386e1cd18fcd58dbbb65cc9
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-09 16:21:01 +0300

    add hood model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
+class Hood(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=1000)
+    occupants = models.CharField(max_length=500)
+    admin = models.ForeignKey(admin)
