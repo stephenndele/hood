53cfb6d66fc0a3c5274a618f13028fa2da8e44d5
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-09 21:30:55 +0300

    add hood funtion that returns all hoods

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from models import Hood
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'main/index.html')
+    query = request.GET.get('title')    
+    
+    allHoods = None
+    if query:
+        allHoods = Hood.objects.filter(title__icontains=query)
+    else:
+
+        allHoods = Hoods.objects.all()
+    context = {
+        "hoods": allHoods,
+    }
+
+    return render(request,'main/index.html', context)
