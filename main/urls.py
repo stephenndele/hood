7350aeac4ac1ef4de53e2f3308ee301700b86903
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-10 13:48:01 +0300

    add business url

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("user/", views.userpage, name = "userpage"),
     path('<hood_id>/new-post', views.create_post, name='post'),
     path('details/<int:id>', views.details, name='details'),
+    path('<hood_id>/new-business', views.create_business, name='business'),
 
 ]
