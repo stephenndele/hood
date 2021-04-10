e092f6da0ea564243e606a6d387fe55225fe73d5
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-10 10:45:13 +0300

    add profile model

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ class Hood(models.Model):
     occupants = models.CharField(max_length=500)
     image = models.URLField(
         default='default.jpg')
-    # admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hoods')
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
 
@@ -25,22 +25,22 @@ class Hood(models.Model):
         self.delete()
 
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     name = models.CharField(max_length=80, blank=True)
-#     bio = models.TextField(max_length=254, blank=True)
-#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
-#     location = models.CharField(max_length=50, blank=True, null=True)
-#     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    hood = models.ForeignKey(Hood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
-#     def __str__(self):
-#         return f'{self.user.username} profile'
+    def __str__(self):
+        return f'{self.user.username} profile'
 
-#     @receiver(post_save, sender=User)
-#     def create_user_profile(sender, instance, created, **kwargs):
-#         if created:
-#             Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-#     @receiver(post_save, sender=User)
-#     def save_user_profile(sender, instance, **kwargs):
-#         instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
--- main/views.py
+++ main/views.py
@@ -34,3 +34,26 @@ def add_hood(request):
     else:
         form = HoodForm()
     return render(request, 'main/addhoods.html', {'form': form, "controller":"Add Hood"}) 
+
+# def details(request, id):
+#     hood = Hood.objects.get(id=id)
+#     reviews = Review.objects.filter(project=id).order_by('-comment')
+    
+#     average1 = reviews.aggregate(Avg("design_rating"))["design_rating__avg"]
+#     average2 = reviews.aggregate(Avg("usability_rating"))["usability_rating__avg"]
+#     average3 = reviews.aggregate(Avg("content_rating"))["content_rating__avg"]
+
+#     average = (average1 + average2 + average3) / 3
+
+#     if average == None:
+#         average = 0
+#     average = round(average, 2)
+
+#     context = {
+#         "project": project,
+#         "reviews": reviews,
+#         "average": average,
+
+#     }
+
+#     return render( request,'main/details.html', context)
