2d69fba6a812a537f7455d2e17bef6de0def45d4
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-10 10:23:41 +0300

    add contacts details on hood model

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Hood(models.Model):
@@ -9,4 +11,36 @@ class Hood(models.Model):
     occupants = models.CharField(max_length=500)
     image = models.URLField(
         default='default.jpg')
-    # admin = models.ForeignKey('Admin', on_delete=models.CASCADE)
+    # admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     name = models.CharField(max_length=80, blank=True)
+#     bio = models.TextField(max_length=254, blank=True)
+#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
+#     location = models.CharField(max_length=50, blank=True, null=True)
+#     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+#     def __str__(self):
+#         return f'{self.user.username} profile'
+
+#     @receiver(post_save, sender=User)
+#     def create_user_profile(sender, instance, created, **kwargs):
+#         if created:
+#             Profile.objects.create(user=instance)
+
+#     @receiver(post_save, sender=User)
+#     def save_user_profile(sender, instance, **kwargs):
+#         instance.profile.save()
